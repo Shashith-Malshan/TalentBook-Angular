1731332a42dbbd7002567ef5af35962f136a7579
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-20 17:39:13 +0530

    add shedule-interview component

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.css',
 })
 export class NavBar {
+  
 
 }
---
+++ src/app/components/schedule-interview/schedule-interview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-interview',
+  imports: [],
+  templateUrl: './schedule-interview.html',
+  styleUrl: './schedule-interview.css',
+})
+export class ScheduleInterview {
+
+}
