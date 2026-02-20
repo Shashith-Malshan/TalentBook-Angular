dc4c9628029c8412b71110c4f196f9545db482d5
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-20 19:17:14 +0530

    create shedule page

---
+++ src/app/pages/shedule/shedule.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shedule',
+  imports: [],
+  templateUrl: './shedule.html',
+  styleUrl: './shedule.css',
+})
+export class Shedule {
+
+}
