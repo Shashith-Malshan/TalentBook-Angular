2408b2d2b8882153fdec0e9a0e5079e1346bf3e8
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-20 17:39:48 +0530

    add book-interview component

---
+++ src/app/components/book-interview/book-interview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-interview',
+  imports: [],
+  templateUrl: './book-interview.html',
+  styleUrl: './book-interview.css',
+})
+export class BookInterview {
+
+}
